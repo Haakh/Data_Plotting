8d5ee27eaef2bc14deaf66e9ed91eb93efe8bde5
BHAVISH <hakbhavish@gmail.com>
2015-05-09 17:54:46 +0530

    Made Plot 2

---
+++ plot2.R
@@ -0,0 +1,6 @@
+consume <- read.csv("c.txt", sep=";", na.strings="?")
+consume <- subset(consume, Date == "1/2/2007" | Date == "2/2/2007")
+time <- strptime(paste(consume$Date, consume$Time), format="%d/%m/%Y %H:%M:%S")
+png("plot2.png", width=480, height=480)
+plot(x=time, y=consume[,"Global_active_power"], type="l",ylab="Global Active Power (kilowatts)", main="", xlab="")
+dev.off()
