1fe7c6e2bf5fed502cda3f8fb264a9c259dac3f1
Bhavish <hakbhavish@gmail.com>
2015-05-09 18:07:09 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,5 @@
+## The File downloaded is extracted first, and then name changed to c.txt for ease
+#It's read as a csv file for easy conversion.
 consume <- read.csv("c.txt", sep=";", na.strings="?")
 consume <- subset(consume, Date == "1/2/2007" | Date == "2/2/2007")
 consume[, "Date"] <- as.Date(strptime(consume[, "Date"], format='%d/%m/%Y'))
