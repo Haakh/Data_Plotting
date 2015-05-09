77a6a644fc34ec9470875894c27c7fb17fb75895
Bhavish <hakbhavish@gmail.com>
2015-05-09 18:10:15 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,9 +1,12 @@
+## The File downloaded is extracted first, and then name changed to c.txt for ease
+#It's read as a csv file for easy conversion.
 consume <- read.csv("household_power_consumption.csv", sep=";", na.strings="?")
 consume <- subset(consume, Date == "1/2/2007" | Date == "2/2/2007")
+
 time <- strptime(paste(consume$Date, consume$Time), format="%d/%m/%Y %H:%M:%S")
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2))
-
+# 4 plots are plotted in the same image in clockwise manner...
 plot(x=time, y=consume[,"Global_active_power"], type="l",ylab="Global Active Power", main="", xlab="")
 plot(x=time, y=consume[,"Voltage"], type="l",ylab="Voltage", main="", xlab="datetime")
 
